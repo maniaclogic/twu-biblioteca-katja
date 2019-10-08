f292e5156955ea8a451d3b4ce4bc4a4663a71d9c
Katja Bensiek <katben@Katjas-MacBook-Pro.local>
2019-10-08 09:03:44 +0200

    [Katja][1.1]<failing test for app.greet()>

---
+++ test/BibliotecaAppTest.java
@@ -0,0 +1,14 @@
+import com.twu.biblioteca.BibliotecaApp;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class BibliotecaAppTest {
+
+    @Test
+    public void testGreet () {
+        BibliotecaApp app = new BibliotecaApp();
+        assertThat(app.greet(), is("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!"));
+    }
+}
