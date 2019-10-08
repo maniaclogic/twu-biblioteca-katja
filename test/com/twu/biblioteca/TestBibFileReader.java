4f08afbcd766f48f43167e00bfda264d5ef45843
maniaclogic <katben@Katjas-MacBook-Pro.local>
2019-10-08 12:00:19 +0200

    [Katja][1.3]<Test Year included - fails>

--- test/com/twu/biblioteca/TestBibFileReader.java
+++ test/com/twu/biblioteca/TestBibFileReader.java
@@ -1,15 +1,16 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.*;
 
 public class TestBibFileReader {
-
 
     @Test
     public void TestReaderResultNotNull() throws Exception {
@@ -23,5 +24,15 @@ public class TestBibFileReader {
         BibFileReader reader = new BibFileReader();
 
         assertThat(reader.read(), instanceOf(ArrayList.class));
+    }
+
+    @Test
+    public void TestYearIncluded() throws Exception {
+        BibFileReader reader = new BibFileReader();
+        ArrayList books = reader.read();
+        String bookExample = books.get(1).toString();
+
+        assertThat(bookExample, containsString("1"));
+
     }
 }
