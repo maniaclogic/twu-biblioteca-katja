63ba8fd801643d7059e074b6ce8f3a61f7fbbc7c
maniaclogic <katben@Katjas-MacBook-Pro.local>
2019-10-10 10:38:17 +0200

    [Katja][2.1]<Realised Movie needs rating and director properties. Fixed.>

--- src/com/twu/biblioteca/Movie.java
+++ src/com/twu/biblioteca/Movie.java
@@ -7,22 +7,30 @@ public class Movie implements Media {
     private String name;
     private int year;
     private boolean checkedOut = false;
+    private double rating;
+    private String director;
     public static ArrayList<Movie> instances = new ArrayList<Movie>();
 
-    private Movie(String name, int year) {
+    private Movie(String name, int year, double rating, String director) {
         this.name = name;
         this.year = year;
+        this.rating= rating;
+        this.director = director;
+
     }
 
     private Movie(String movieInfo) {
         String[] parts = movieInfo.split(" \\(");
-        String[] figment = parts[1].split("\\)");
+        String[] figment = parts[1].split("\\) ");
+        String[] piece = figment[1].split("; ");
         this.year = Integer.parseInt(figment[0]);
         this.name = parts[0];
+        this.rating = Double.parseDouble(piece[0]);
+        this.director = piece[1];
     }
 
-    public static Movie createMovie(String name, int year) {
-        Movie movie = new Movie(name, year);
+    public static Movie createMovie(String name, int year, double rating, String director) {
+        Movie movie = new Movie(name, year, rating, director);
         instances.add(movie);
         return movie;
     }
@@ -45,17 +53,24 @@ public class Movie implements Media {
         return this.checkedOut;
     }
 
+    public double getRating() {
+        return this.rating;
+    }
+
+    public String getDirector() {
+        return this.director;
+    }
+
     public static ArrayList<Movie> getInstances() {
         return instances;
     }
 
     public String toString() {
-
-        return this.getName() +" ("+ this.getYear() +")";
+        return this.getName() +" ("+ this.getYear() +")     "+ this.getRating()+"    " + this.getDirector();
     }
 
     @Override
-    public void checkOut(){
+    public void checkOut() {
 
     }
 }
