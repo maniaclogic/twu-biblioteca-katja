c909516d209a58003ef38232d3fd58f63308c80b
maniaclogic <katben@Katjas-MacBook-Pro.local>
2019-10-10 17:01:13 +0200

    [Katja][2.4]<Impl. getUserContacts>

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -11,8 +11,8 @@ public class BibliotecaApp {
         Helper.readBooks("/Users/katben/Documents/TWU_Biblioteca-master/src/com/twu/biblioteca/bookList.txt");
         Helper.readMovies("/Users/katben/Documents/TWU_Biblioteca-master/src/com/twu/biblioteca/movielist.txt");
 
-        User user2 = User.createUser("123-1234", "1234");
-        User user3 = User.createUser("123-4567", "1234");
+        User user2 = User.createUser("123-1234", "1234", "Tommy", "0149-6698584", "jfh@jdhs.com");
+        User user3 = User.createUser("123-4567", "1234", "Jon", "948372938272", "jfu@dosmw.com");
 
         System.out.println(menu.greet());
 
--- src/com/twu/biblioteca/User.java
+++ src/com/twu/biblioteca/User.java
@@ -1,24 +1,27 @@
 package com.twu.biblioteca;
 
+import java.security.PublicKey;
 import java.util.ArrayList;
 
 public class User {
     private String idNum;
     private String psswd;
+    private String name;
+    private String tel;
+    private String email;
     private ArrayList<String> checkedOutMedia = new ArrayList<String>();
     public static ArrayList<User> userList = new ArrayList<User>();
 
-    User(String idNum, String psswd) {
-        if (idNum.length() == 8 && idNum.contains("-")) {
-            this.idNum = idNum;
-        }
-        if (psswd.length() > 3) {
-            this.psswd = psswd;
-        }
+    User(String idNum, String psswd, String name, String tel, String email) {
+        if (idNum.length() == 8 && idNum.contains("-")) { this.idNum = idNum; }
+        if (psswd.length() > 3) { this.psswd = psswd; }
+        if (tel.length() > 8) { this.tel = tel; }
+        if (email.contains("@") && email.contains(".")) { this.email = email; }
+        this.name = name;
     }
 
-    public static User createUser(String idNum, String psswd) {
-        User user = new User(idNum, psswd);
+    public static User createUser(String idNum, String psswd, String name, String tel, String email) {
+        User user = new User(idNum, psswd, name, tel, email);
         userList.add(user);
         return user;
     }
@@ -27,15 +30,24 @@ public class User {
 
     public String getPsswd() { return this.psswd; }
 
+    public String getName() { return this.name; }
+
+    public String getNumber() { return this.tel; }
+
+    public String getEmail() { return this.email; }
+
     public ArrayList<String> getCheckedOutMedia() { return this.checkedOutMedia; }
 
     public static ArrayList<User> getUserList() { return userList; }
 
-    public String toString() {
-        return this.idNum + ";" + this.psswd;
-    }
+    public String toString() { return this.idNum + ";" + this.psswd; }
 
     public void checkedOut(Media media) {
         checkedOutMedia.add(media.toString());
     }
+
+    String getUserContacts() {
+        return "ID: "+this.getIdNum()+ ", Name: "+this.getName()+", Tel: "+this.getNumber()+", Email: "+this.getEmail();
+    }
+    
 }
